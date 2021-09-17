1e5cdf195eed54ddf3eb96de783e4c5d6d344738
Khairul Hasan <khairul18103199@gmail.com>
2021-09-17 11:44:26 +0600

    First Commit

---
+++ shop/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='shop'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('tracker/', views.tracker, name='tracker'),
+    path('search/', views.search, name='search'),
+    path('products/<int:myid>', views.products, name='products'),
+    path('checkout/', views.checkout, name='checkout'),
+]
